4ecb0e9feec870951b7ff8716663b2f25391eaed
victor <uranus_0831@qq.com>
2026-08-12 22:37:24 +0800

    再次尝试ssh链接

--- 数模大赛/数据可视化.py
+++ 数模大赛/数据可视化.py
@@ -1,4 +1,7 @@
 import matplotlib.pyplot as plt  # 导入matplotlib.pyplot模块，用于绘制图表
 import seaborn as sns # 导入seaborn模块，用于数据可视化
 #1.解决中文乱码，必须加
-plt.rcParams[]
+plt.rcParams['font.sans-serif'] = ['SimHei']  # 设置字体为SimHei以支持中文显示
+plt.rcParams['axes.unicode_minus'] = False  # 正常显示负号
+#2.设置图表大小
+plt.figure(figsize=(10, 6))  # 设置图表大小为10英寸宽，6英寸高
